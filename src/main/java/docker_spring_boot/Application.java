1db7e01c8619dedb8ab588686b003ddfd799c742
tankui <tankui@maimaiti.cn>
2020-01-07 17:13:03 +0800

    commit1

--- src/main/java/docker_spring_boot/Application.java
+++ src/main/java/docker_spring_boot/Application.java
@@ -5,19 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * 主应用入口
- * @author <a href="http://waylau.com">waylau.com</a>
- * @date 2016年3月19日
- */
 @SpringBootApplication
 @RestController
 public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World."
-				+ "<br />Welcome to <a href='http://waylau.com'>waylau.com</a></li>";
+		return "Hello World V1!";
 	}
 
 	public static void main(String[] args) {
