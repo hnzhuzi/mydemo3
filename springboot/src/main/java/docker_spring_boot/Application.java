d06109bbeca7f394f0cab7c3c4ae050ec6794ff6
tankui <tankui@maimaiti.cn>
2020-03-11 17:49:46 +0800

    v10

--- springboot/src/main/java/docker_spring_boot/Application.java
+++ springboot/src/main/java/docker_spring_boot/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello World V9!";
+		return "Hello World V10!";
 	}
 
 	public static void main(String[] args) {
