010d9414833e9367f86d9be9fba581f426c45f4e
hnzhuzi <you@example.com>
2021-02-19 15:19:06 +0800

    v2

--- springboot/src/main/java/docker_spring_boot/Application.java
+++ springboot/src/main/java/docker_spring_boot/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello World V1!";
+		return "Hello World V2!";
 	}
 
 	public static void main(String[] args) {
